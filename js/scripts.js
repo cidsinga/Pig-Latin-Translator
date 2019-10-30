949e7c9852787f539aa00493e7e659df46df2065
Student <example@example.com>
2019-10-30 16:12:47 -0700

    add for loop to account for multiple consonants

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,29 @@ $(document).ready(function() {
     var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
     var pig = words.map(function(word) {
-      if (vowels.includes(word[0])) {
-        return(word + "way")
-      } else {
-        return (word.slice(1) + word[0] + "ay")
-      };
-    });
-    console.log(pig)
-  });
-});
+     if (vowels.includes(word[0])) {
+       return(word + "way")
+     } else {
+       for (var i = 0; i < word.length; i++) {
+         if (vowels.includes(word[i])) {
+           return(word.slice(i) + word.slice(0,i) + "ay");
+       }
+     }
+   }
+
+
+       // return (word.slice(1) + word[0] + "ay")
+     });
+      console.log(pig)
+   });
+ });
+
+
+
+
+
+
+
+// for (var i = 0; i < word.length; i++) {
+//   if (vowels.includes(word[i])) {
+//     return(word.slice(i) + word.slice(0,i) + "ay");
